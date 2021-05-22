c21e8b36b7e351309041b4a6430d5efe176bcf37
James Turk <dev@jamesturk.net>
2021-05-22 15:23:17 -0400

    add staff page

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import csv
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,3 +12,12 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/staff")
+def staff():
+    # thanks to http://www.figmentfly.com/bb/badguys3.html for names
+    with open("data/employees.csv") as f:
+        employees = list(csv.DictReader(f))
+
+    return render_template("staff.html", employees=employees)
