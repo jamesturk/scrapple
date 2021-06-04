1cb031c3b8eb5f82fdfbd9ee23c0191094662b09
James Turk <dev@jamesturk.net>
2021-06-04 17:49:42 -0400

    add fixes for tutorial part 2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import csv
-from flask import Flask, render_template, abort
+import math
+from flask import Flask, render_template, abort, request
 
 app = Flask(__name__)
 
@@ -27,7 +28,21 @@ def about():
 
 @app.route("/staff")
 def staff():
-    return render_template("staff.html", employees=employees().values())
+    page = int(request.args.get("page", 1))
+    per_page = 10
+    total_items = len(employees())
+    max_page = math.ceil(total_items / per_page)
+    print(max_page)
+    if page < 1 or page > max_page:
+        abort(404)
+    page_employees = list(employees().values())[(page - 1) * per_page : page * per_page]
+    return render_template(
+        "staff.html",
+        employees=page_employees,
+        page=page,
+        prev_page=page - 1,
+        next_page=page + 1 if page < max_page else None,
+    )
 
 
 @app.route("/staff/<id_>")
